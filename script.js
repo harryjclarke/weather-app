139648d0165fad8e1bf7ecd21cc88ec5c464b98c
unknown <138071419+harryjclarke@users.noreply.github.com>
2024-01-23 19:07:44 +0000

    Add preliminary styling

--- script.js
+++ script.js
@@ -9,17 +9,30 @@ const fetchWeather = async (city) => {
       `https://api.weatherapi.com/v1/current.json?key=96c39487c1cf443eaef194818242201&q=${city}`
     );
     const weather = await response.json();
-    output.style.display = 'block'
-    document.getElementById('location').innerHTML = `${weather.location.name}, ${weather.location.country}`
-    document.getElementById('time').innerHTML = `${weather.location.localtime}`
-    document.getElementById('temp').innerHTML = `${weather.current.temp_c}°C` 
-    document.getElementById('temp-feels').innerHTML = `Feels like ${weather.current.feelslike_c}°C`
-    document.getElementById('humidity').innerHTML = `${weather.current.humidity}` 
-    document.getElementById('precipitation').innerHTML = `${weather.current.precip_mm}` 
-    document.getElementById('wind').innerHTML = `${weather.current.wind_mph} mph` 
+    output.style.display = 'block';
+    document.getElementById(
+      'location'
+    ).innerHTML = `${weather.location.name}, ${weather.location.country}`;
+    document.getElementById('time').innerHTML = `${weather.location.localtime}`;
+    document.getElementById('temp').innerHTML = `${weather.current.temp_c}°C`;
+    document.getElementById(
+      'temp-feels'
+    ).innerHTML = `Feels like ${weather.current.feelslike_c}°C`;
+    document.getElementById(
+      'condition'
+    ).innerHTML = `${weather.current.condition.text}`;
+    document.getElementById(
+      'humidity'
+    ).innerHTML = `${weather.current.humidity}% Humidity`;
+    document.getElementById(
+      'precipitation'
+    ).innerHTML = `${weather.current.precip_mm}% Precipitation`;
+    document.getElementById(
+      'wind'
+    ).innerHTML = `${weather.current.wind_mph} MPH Wind`;
   } catch (error) {
-      errorMsg.innerHTML = 'Please enter a valid city'
-      output.style.display = 'none'
+    errorMsg.innerHTML = 'Please enter a valid city';
+    output.style.display = 'none';
     console.log(error);
   }
 };
